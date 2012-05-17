d28e2eef57c658a05302ddde8f51f59f70b1fc4a
Louis-Philippe Huberdeau <lp@huberdeau.info>
2012-05-17 09:39:36 -0400

    Switching to composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,18 +1,6 @@
 <?php
 
-spl_autoload_register(function ($className) {
-    $parts = explode('\\', $className);
-
-    if (reset($parts) == 'HireVoice') {
-        require __DIR__ . '/lib/' . implode('/', $parts) . '.php';
-    } elseif (reset($parts) == 'Entity') {
-        require __DIR__ . '/tests/' . implode('/', $parts) . '.php';
-    } elseif (reset($parts) == 'Doctrine') {
-        require __DIR__ . '/vendor/doctrine-common/lib/' . implode('/', $parts) . '.php';
-    } elseif (reset($parts) == 'Everyman') {
-        require __DIR__ . '/vendor/neo4jphp/lib/' . implode('/', $parts) . '.php';
-    }
-});
+require __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Entity.php';
 require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Auto.php';
